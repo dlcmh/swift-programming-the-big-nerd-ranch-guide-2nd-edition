8e38415d8a938cf1d5cfe7c4a73e4a674a20f69b
David Chin <dlcmhd@me.com>
2017-04-18 19:53:57 +0800

    New project

---
+++ chapter-26/VocalTextEdit/VocalTextEdit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VocalTextEdit
+//
+//  Created by David Chin on 18/04/2017.
+//  Copyright © 2017 David Chin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ chapter-26/VocalTextEdit/VocalTextEdit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VocalTextEdit
+//
+//  Created by David Chin on 18/04/2017.
+//  Copyright © 2017 David Chin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
