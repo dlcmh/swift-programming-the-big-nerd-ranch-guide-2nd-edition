2ed8ad16dc4d7e3b3d76e7f6fe6cd1daa79bad13
David Chin <dlcmhd@me.com>
2017-04-18 22:02:26 +0800

    Implement document saving

--- chapter-26/VocalTextEdit/VocalTextEdit/Document.swift
+++ chapter-26/VocalTextEdit/VocalTextEdit/Document.swift
@@ -9,6 +9,16 @@
 import Cocoa
 
 class Document: NSDocument {
+    
+    enum Error: Swift.Error, LocalizedError {
+        case UTF8Encoding
+        
+        var failureReason: String? {
+            switch self {
+            case .UTF8Encoding: return "File cannot be encoded in UTF-8."
+            }
+        }
+    }
 
     override init() {
         super.init()
@@ -29,7 +39,15 @@ class Document: NSDocument {
     override func data(ofType typeName: String) throws -> Data {
         // Insert code here to write your document to data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning nil.
         // You can also choose to override fileWrapperOfType:error:, writeToURL:ofType:error:, or writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+//        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+        let windowController = windowControllers[0]
+        let viewController = windowController.contentViewController as! ViewController
+        let contents = viewController.textView.string ?? ""
+        guard let data = contents.data(using: .utf8) else {
+            throw Document.Error.UTF8Encoding
+        }
+        
+        return data
     }
 
     override func read(from data: Data, ofType typeName: String) throws {
