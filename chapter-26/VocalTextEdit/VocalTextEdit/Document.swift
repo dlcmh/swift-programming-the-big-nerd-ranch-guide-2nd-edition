86908c286a9f8cae6a68c7e17e71dfe1ea56ed59
David Chin <dlcmhd@me.com>
2017-04-18 22:26:38 +0800

    Implement reading content from an opened file

--- chapter-26/VocalTextEdit/VocalTextEdit/Document.swift
+++ chapter-26/VocalTextEdit/VocalTextEdit/Document.swift
@@ -12,13 +12,17 @@ class Document: NSDocument {
     
     enum Error: Swift.Error, LocalizedError {
         case UTF8Encoding
+        case UTFDecoding
         
         var failureReason: String? {
             switch self {
             case .UTF8Encoding: return "File cannot be encoded in UTF-8."
+            case .UTFDecoding: return "Files is not valid UTF-8."
             }
         }
     }
+    
+    var contentFromReadFile: String = "" // stores data read from a file
 
     override init() {
         super.init()
@@ -33,6 +37,11 @@ class Document: NSDocument {
         // Returns the Storyboard that contains your Document window.
         let storyboard = NSStoryboard(name: "Main", bundle: nil)
         let windowController = storyboard.instantiateController(withIdentifier: "Document Window Controller") as! NSWindowController
+        
+        // Populate Text View with contents read from an opened file
+        let viewController = windowController.contentViewController as! ViewController
+        viewController.textView.string = contentFromReadFile
+
         self.addWindowController(windowController)
     }
 
@@ -54,7 +63,17 @@ class Document: NSDocument {
         // Insert code here to read your document from the given data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning false.
         // You can also choose to override readFromFileWrapper:ofType:error: or readFromURL:ofType:error: instead.
         // If you override either of these, you should also override -isEntireFileLoaded to return false if the contents are lazily loaded.
-        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+//        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+        guard let contents = String(data: data, encoding: .utf8) else {
+            throw Document.Error.UTFDecoding
+        }
+//
+//      WARNING: BIG PROBLEM HERE
+//        let windowController = windowControllers[0]
+//        let viewController = windowController.contentViewController as! ViewController
+//        viewController.textView.string = contents
+        
+        contentFromReadFile = contents
     }
 
 
