aba1091a3b0fabc52b0d9211f1650f99a2a0670c
David Chin <dlcmhd@me.com>
2017-04-19 12:43:45 +0800

    Progress indicator with NSProgressIndicator

--- chapter-26/VocalTextEdit/VocalTextEdit/ViewController.swift
+++ chapter-26/VocalTextEdit/VocalTextEdit/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: NSViewController {
     @IBOutlet var textView: NSTextView!
     @IBOutlet weak var speakButton: NSButton! // for setting the enabled property when speech starts or stops
     @IBOutlet weak var stopButton: NSButton! // for setting the enabled property when speech starts or stops
+    @IBOutlet weak var progressIndicator: NSProgressIndicator!
     
     // Refactoring result: `contents` computed property to represent the contents of Text View
     var contents: String? {
@@ -43,6 +44,7 @@ class ViewController: NSViewController {
         if let contents = textView.string, !contents.isEmpty {
             speakButton.isEnabled = false
             stopButton.isEnabled = true
+            progressIndicator.isHidden = false
             speechSynthesizer.startSpeaking(contents)
         } else {
             speakButton.isEnabled = false
@@ -65,6 +67,19 @@ extension ViewController: NSSpeechSynthesizerDelegate {
     func speechSynthesizer(_ sender: NSSpeechSynthesizer, didFinishSpeaking finishedSpeaking: Bool) {
         speakButton.isEnabled = true
         stopButton.isEnabled = false
+        progressIndicator.isHidden = true
+        progressIndicator.doubleValue = 0
+    }
+    
+    func speechSynthesizer(_ sender: NSSpeechSynthesizer, willSpeakWord characterRange: NSRange, of string: String) {
+        let wordStartLocation = characterRange.location
+        let length = characterRange.length
+        let wordEndLocation = wordStartLocation + length
+        let textLength = string.characters.count
+//        let estimatedCompletionBasedOnWordStart = ((Double(wordStartLocation)/Double(textLength)) * 100).rounded(.toNearestOrEven)
+        let estimatedCompletionBasedOnWordEnd = ((Double(wordEndLocation)/Double(textLength)) * 100).rounded(.toNearestOrEven)
+//        print("location: \(wordStartLocation), length: \(length), word end location: \(wordEndLocation), textLength: \(textLength), estimated completion based on word start: \(estimatedCompletionBasedOnWordStart), estimated completion based on word end: \(estimatedCompletionBasedOnWordEnd)")
+        progressIndicator.doubleValue = estimatedCompletionBasedOnWordEnd
     }
 }
 
